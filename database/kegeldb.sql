8750c4521003594115c2da06a6e6e47335fd0582
HannesKes <hannes.kessling@hsw-stud.de>
2019-08-06 19:47:34 +0200

    Datenbank aktualisiert

--- database/kegeldb.sql
+++ database/kegeldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 06. Aug 2019 um 12:36
+-- Erstellungszeit: 06. Aug 2019 um 19:46
 -- Server-Version: 10.1.38-MariaDB
 -- PHP-Version: 7.3.3
 
@@ -29,7 +29,7 @@ USE `kegeldb`;
 --
 -- Tabellenstruktur für Tabelle `users`
 --
--- Erstellt am: 06. Aug 2019 um 10:35
+-- Erstellt am: 06. Aug 2019 um 11:48
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -37,13 +37,22 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(32) COLLATE utf8_german2_ci NOT NULL,
   `password` text COLLATE utf8_german2_ci NOT NULL,
+  `email` varchar(64) COLLATE utf8_german2_ci NOT NULL,
   `firstname` varchar(64) COLLATE utf8_german2_ci NOT NULL,
-  `lastname` varchar(64) COLLATE utf8_german2_ci NOT NULL
+  `lastname` varchar(64) COLLATE utf8_german2_ci NOT NULL,
+  `is_new` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
 
 --
 -- RELATIONEN DER TABELLE `users`:
 --
+
+--
+-- Daten für Tabelle `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `firstname`, `lastname`, `is_new`) VALUES
+(1, 'HanKes', '$2y$10$nvV6tDPxWBTK2CSM4jbILeaw48GUlnh89ernGaatddp.WbsQXxrlC', 'hannes.kessling@gmail.com', 'Hannes', 'Keßling', 0);
 
 --
 -- Indizes der exportierten Tabellen
@@ -63,7 +72,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
