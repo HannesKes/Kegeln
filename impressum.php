3940bc790e742b6164ffb13db3ab6f990689b1e3
HannesKes <hannes.kessling@hsw-stud.de>
2019-08-06 12:52:08 +0200

    weitere Seiten

---
+++ impressum.php
@@ -0,0 +1,13 @@
+<?php
+include_once $_SERVER["DOCUMENT_ROOT"] . '/Kegeln/header.php';
+?>
+
+<main role="main" class="container">
+
+
+
+</main>
+
+<?php
+include_once $_SERVER["DOCUMENT_ROOT"] . '/Kegeln/footer.php';
+?>
