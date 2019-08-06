e69b5531c388c194654fe469cf2e9138cd0fb778
HannesKes <hannes.kessling@hsw-stud.de>
2019-08-06 12:38:06 +0200

    initialisierung Datenbank

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 include_once $_SERVER["DOCUMENT_ROOT"] . '/Kegeln/header.php';
 ?>
 
-		<main role="main" class="container">
+<main role="main" class="container">
 
-		</main>
+  
+
+</main>
 
 <?php
 include_once $_SERVER["DOCUMENT_ROOT"] . '/Kegeln/footer.php';
