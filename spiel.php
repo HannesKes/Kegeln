0698173559a120a501989fc45af2a5fb782c2aa5
HannesKes <49340305+HannesKes@users.noreply.github.com>
2019-08-18 20:36:41 +0200

    Button für nächstes Spiel + richtiger Pumpenkönig

--- spiel.php
+++ spiel.php
@@ -17,15 +17,28 @@ include_once $_SERVER["DOCUMENT_ROOT"] . '/Kegeln/objects/game.php';
   $game->setId($_GET['id']);
   $game->readOne();
 
+  $next = false;
+  $nextId = $_GET['id']+1;
+  $nextGame = new Game($db);
+  $nextGame->setId($nextId);
+  if ($nextGame->readOne()){
+    $next = true;
+  }
+
   $date = $game->getDate();
   $formattedDate = substr($date, 8, 2) . "." . substr($date, 5, 2) . "." . substr($date, 0, 4);
 
   $user = new User($db);
-  $user->setId($game->getId());
+  $user->setId($game->getKing());
   $user->readOne();
   $king = $user->getUsername() . " (" . $user->getFirstname() . " " . $user->getLastname() . ")";
 
-  if(isset($_GET['id'])){
+  if (isset($_GET['id'])) {
+
+    if ($next) {
+      echo "<a href='spiel.php?id=$nextId'><button class='btn btn-secondary float-right'>nächstes Spiel</button></a>";
+    }
+
   ?>
 
     <u style="color: #5DC3FE"><center><h2 style="color: #FE01DC">Spiel vom <b><?php echo $formattedDate;?></b></h2></center></u><br/><br/>
