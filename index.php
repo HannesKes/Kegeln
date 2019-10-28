16369fa5a1dfc4941958f64b75e11d30167001d1
HannesKes <49340305+HannesKes@users.noreply.github.com>
2019-10-28 22:41:01 +0100

    Gif auf Startseite

--- index.php
+++ index.php
@@ -12,11 +12,24 @@ include_once $_SERVER["DOCUMENT_ROOT"] . '/Kegeln/objects/session.php';
 
   include_once $_SERVER["DOCUMENT_ROOT"] . '/Kegeln/header.php';
 
+  // put gifs in array to shuffle an pick a random
+  $gif_array[] = "https://media.giphy.com/media/KoE4dzVStw9RS/giphy.gif";
+  $gif_array[] = "https://media.giphy.com/media/xldwLDL59sBWg/giphy.gif";
+  $gif_array[] = "http://giphygifs.s3.amazonaws.com/media/3utCNx5N58Gnm/giphy.gif";
+  $gif_array[] = "https://media.giphy.com/media/yoJC2ElACT9uejhvYk/giphy.gif";
+  $gif_array[] = "https://media.giphy.com/media/FYMc9CFlptI8o/giphy.gif";
+  $gif_array[] = "http://giphygifs.s3.amazonaws.com/media/1flAwtHCYosL6LWnHr/giphy.gif";
+
+  shuffle($gif_array);
+
+  // show random gif
+  echo "<div style='display: flex; justify-content: center; align-items: center'>";
+    echo "<img src='$gif_array[0]' />";
+  echo "</div>";
 
   //  style="background-image: url('Kegeln/media/kegeln.jpg'); background-color: 'CCCCCC'"
 
 ?>
-
 
   <!-- Background? Funzt nicht, ist schon im style.css -->
 
