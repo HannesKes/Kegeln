ced0fe9abd5b6fde52e8feed7aff23e0e17db75c
HannesKes <49340305+HannesKes@users.noreply.github.com>
2019-11-03 01:24:23 +0100

    Footer angepasst

--- footer.php
+++ footer.php
@@ -1,34 +1,62 @@
 </main>
 <br/>
-<footer id="myFooter" class="footer font-small fixed-bottom" style="background-color: #353535;">
-  <div class="container">
-    <div class="row">
-      <!-- <div class="col-3"> -->
-        <!-- no text in first column -->
-      <!-- </div> -->
-      <div class="col-6">
-        <!-- symbols -->
-        <a href="https://www.instagram.com/die_bierpumpen/" class="fa fa-instagram instagram"></a>
-        <a href="https://twitter.com/hopfenspeicher" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a>
-        <a href="https://de-de.facebook.com/ichliebebier.at/" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a>
-        <a href="https://www.youtube.com/watch?v=OXPswg0urtQ&t=2s" target="_blank" class="google"><i class="fa fa-youtube"></i></a>
-        <a href="https://www.reddit.com/r/beer/" target="_blank" class="reddit"><i class="fa fa-reddit-alien"></i></a>
-        <a href="https://www.snapchat.com/add/hopfenspeicher" target="_blank" class="snapchat"><i class="fa fa-snapchat"></i></a>
-        <a href="https://bierpong.de.cool/" target="_blank" class="beer"><i class="fa fa-beer"></i></a>
-      </div>
-      <div class="col-2">
-        <a href="/Kegeln/impressum.php">Impressum</a>
-      </div>
-      <div class="col-4">
-        <!-- Copyright -->
-        <div class="footer-copyright text-center text-muted">© 2019 Copyright
-          <a class="text-white"href="https://bierpumpen.de">  Bierpumpen.de</a>
+
+<!-- content for larger devices -->
+<div class="d-none d-sm-block">
+  <footer id="myFooter" class="footer font-small fixed-bottom">
+    <div class="container">
+      <div class="row">
+        <!-- <div class="col-3"> -->
+          <!-- no text in first column -->
+        <!-- </div> -->
+        <div class="col-4">
+          <!-- symbols -->
+          <a href="https://www.instagram.com/die_bierpumpen/" class="fab fa-instagram instagram pr-3" style="font-size: 22px"></a>
+          <a href="https://twitter.com/hopfenspeicher" target="_blank" class="twitter"><i class="fab fa-twitter pr-3" style="font-size: 22px"></i></a>
+          <a href="https://de-de.facebook.com/ichliebebier.at/" target="_blank" class="facebook"><i class="fab fa-facebook pr-3" style="font-size: 22px"></i></a>
+          <a href="https://www.youtube.com/watch?v=OXPswg0urtQ&t=2s" target="_blank" class="google"><i class="fab fa-youtube pr-3" style="font-size: 22px"></i></a>
+          <a href="https://www.reddit.com/r/beer/" target="_blank" class="reddit"><i class="fab fa-reddit-alien pr-3" style="font-size: 22px"></i></a>
+          <a href="https://www.snapchat.com/add/hopfenspeicher" target="_blank" class="snapchat"><i class="fab fa-snapchat pr-3" style="font-size: 22px"></i></a>
+          <a href="https://bierpong.de.cool/" target="_blank" class="beer"><i class="fas fa-beer" style="font-size: 22px"></i></a>
+        </div>
+        <div class="col-2">
+          <a href="/Kegeln/impressum.php">Impressum</a>
+        </div>
+        <div class="col-6">
+          <!-- Copyright -->
+          <div class="footer-copyright text-center text-muted">© 2019 Copyright
+            <a class="text-white"href="https://bierpumpen.de">  Bierpumpen.de</a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</footer>
+  </footer>
+</div>
 
+<!-- content for mobile devices -->
+<div class="d-sm-none px-2">
+  <footer id="myFooter" class="footer font-small fixed-bottom">
+    <div class="container">
+      <div class="row">
+        <a class="col-4" href="/Kegeln/impressum.php">Impressum</a>
+        <!-- Copyright -->
+        <div class="col-8 float-right footer-copyright text-center text-muted">© 2019 Copyright
+          <a class="text-white"href="https://bierpumpen.de">  Bierpumpen.de</a>
+        </div>
+      </div>
+      <div class="float-right">
+        <!-- symbols -->
+        <a href="https://www.instagram.com/die_bierpumpen/" class="fab fa-instagram instagram pr-3" style="font-size: 22px"></a>
+        <a href="https://twitter.com/hopfenspeicher" target="_blank" class="twitter"><i class="fab fa-twitter pr-3" style="font-size: 22px"></i></a>
+        <a href="https://de-de.facebook.com/ichliebebier.at/" target="_blank" class="facebook"><i class="fab fa-facebook pr-3" style="font-size: 22px"></i></a>
+        <a href="https://www.youtube.com/watch?v=OXPswg0urtQ&t=2s" target="_blank" class="google"><i class="fab fa-youtube pr-3" style="font-size: 22px"></i></a>
+        <a href="https://www.reddit.com/r/beer/" target="_blank" class="reddit"><i class="fab fa-reddit-alien pr-3" style="font-size: 22px"></i></a>
+        <a href="https://www.snapchat.com/add/hopfenspeicher" target="_blank" class="snapchat"><i class="fab fa-snapchat pr-3" style="font-size: 22px"></i></a>
+        <a href="https://bierpong.de.cool/" target="_blank" class="beer"><i class="fas fa-beer" style="font-size: 22px"></i></a>
+      </div>
+    </div>
+  </footer>
+</div>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
