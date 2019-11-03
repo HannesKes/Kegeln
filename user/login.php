7c7073efeea6999350cddfb5d7ff4b88d268c3a4
HannesKes <49340305+HannesKes@users.noreply.github.com>
2019-11-04 00:04:22 +0100

    Entfernen des alten Codes von Login + SignUp

--- user/login.php
+++ user/login.php
@@ -28,60 +28,6 @@ include_once $_SERVER["DOCUMENT_ROOT"] . '/Kegeln/objects/session.php';
   }
 ?>
 
-<!-- <div class="container w-login">
-  <div id="loginbox" class="mainbox justify-content-center">
-    <div class="panel panel-info" >
-      <div class="panel-heading">
-        <div class="panel-title font-weight-bold">Login</div>
-        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Passwort vergessen?</a></div>
-      </div>
-
-      <div style="padding-top:30px" class="panel-body" >
-
-        <form id="loginform" class="form-horizontal needs-validation" method="POST" role="form" novalidate>
-
-          <div style="margin-bottom: 25px" class="input-group">
-            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-            <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="Username" maxlength="32" required>
-          </div>
-
-          <div style="margin-bottom: 25px" class="input-group">
-            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-            <input id="login-password" type="password" class="form-control" name="password" placeholder="Passwort" maxlength="32" required>
-          </div>
-
-          <div class="input-group">
-            <div class="checkbox">
-              <label>
-                <input id="login-remember" type="checkbox" name="remember" value="1"> Cookies nutzen und eingeloggt bleiben
-              </label>
-            </div>
-          </div>
-
-          <div style="margin-top:10px" class="form-group">
-            <div class="col-sm-12 controls">
-              <button id="login-btn" type="submit" name="submit" class="btn btn-success">Login</button>
-            </div>
-          </div>
-
-          <div class="form-group">
-            <div class="col-md-12 control">
-              <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
-                Du hast noch keinen Account?
-                <a href="/Kegeln/user/signup.php">
-                Hier registrieren! (Nur für Bierpumpen...)
-                </a>
-              </div>
-            </div>
-          </div>
-
-        </form>
-
-      </div>
-    </div>
-  </div>
-</div> -->
-
 <div class="container">
     <div class="row">
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
--- user/signup.php
+++ user/signup.php
@@ -36,80 +36,6 @@ include_once $_SERVER["DOCUMENT_ROOT"] . '/Kegeln/objects/session.php';
   }
 ?>
 
-<!-- <div class="container w-login">
-  <div id="signupbox" class="mainbox justify-content-center">
-    <div class="panel panel-info">
-      <div class="panel-heading">
-        <a id="signinlink" class="btn btn-sm btn-info float-right" href="/Kegeln/user/login.php">Login</a>
-        <div class="panel-title font-weight-bold">Registrieren</div>
-      </div>
-
-      <div style="padding-top:30px" class="panel-body" >
-          <form id="signupform" class="form-horizontal needs-validation" role="form" method="post" novalidate>
-
-              <div class="form-group row">
-                <div class="col-md-3">
-                    <label for="email" class="col-md-3 control-label">E-Mail</label>
-                </div>
-                <div class="col-md-9">
-                    <input type="text" class="form-control" name="email" placeholder="E-Mail Addresse" maxlength="64" required>
-                </div>
-                <div class="valid-feedback">Korrekt.</div>
-                <div class="invalid-feedback">Bitte fülle dieses Feld aus.</div>
-              </div>
-
-              <div class="form-group row">
-                <div class="col-md-3">
-                    <label for="firstname" class="col-md-3 control-label">Vorname</label>
-                </div>
-                <div class="col-md-9">
-                    <input type="text" class="form-control" name="firstname" placeholder="Max" maxlength="64" required>
-                </div>
-                <div class="valid-feedback">Korrekt.</div>
-                <div class="invalid-feedback">Bitte fülle dieses Feld aus.</div>
-              </div>
-              <div class="form-group row">
-                <div class="col-md-3">
-                    <label for="lastname" class="col-md-3 control-label">Nachname</label>
-                </div>
-                <div class="col-md-9">
-                    <input type="text" class="form-control" name="lastname" placeholder="Mustermann" maxlength="64" required>
-                </div>
-                <div class="valid-feedback">Korrekt.</div>
-                <div class="invalid-feedback">Bitte fülle dieses Feld aus.</div>
-              </div>
-              <div class="form-group row">
-                <div class="col-md-3">
-                    <label for="lastname" class="col-md-3 control-label">Username</label>
-                </div>
-                <div class="col-md-9">
-                    <input type="text" class="form-control" name="username" placeholder="z. B.: maxmuster123" maxlength="32" required>
-                </div>
-                <div class="valid-feedback">Korrekt.</div>
-                <div class="invalid-feedback">Bitte fülle dieses Feld aus.</div>
-              </div>
-              <div class="form-group row">
-                <div class="col-md-3">
-                    <label for="password" class="col-md-3 control-label">Passwort</label>
-                </div>
-                <div class="col-md-9">
-                    <input type="password" class="form-control" name="password" placeholder="sicheres Passwort" maxlength="32" required>
-                </div>
-                <div class="valid-feedback">Korrekt.</div>
-                <div class="invalid-feedback">Bitte fülle dieses Feld aus.</div>
-              </div>
-
-              <div class="form-group">
-                <div class="col-md-offset-3 col-md-9">
-                  <button id="btn-signup" type="submit" name="submit" class="btn btn-success"><i class="far fa-hand-point-right"></i>&nbsp Registrieren</button>
-                </div>
-              </div>
-            </form>
-         </div>
-      </div>
-   </div>
-</div> -->
-
 <div class="container">
     <div class="row">
       <div class="col-lg-10 col-xl-9 mx-auto">
@@ -157,28 +83,6 @@ include_once $_SERVER["DOCUMENT_ROOT"] . '/Kegeln/objects/session.php';
       </div>
     </div>
   </div>
-
-
-<!-- <script>
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-</script> -->
 
 <?php
 include_once $_SERVER["DOCUMENT_ROOT"] . '/Kegeln/footer.php';
